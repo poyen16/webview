87ebf9d0eb19a2aad6d0552dc2d89f043eaceb64
poyen16 <boncor16@gmail.com>
2022-07-07 09:30:00 +0800

    Initial Commit

---
+++ Shared/webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  webviewApp.swift
+//  Shared
+//
+//  Created by boncor chen on 2022/7/7.
+//
+
+import SwiftUI
+
+@main
+struct webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
